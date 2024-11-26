e065451d3b6e8af9146f7ee4da78721a22f27c9f
mmax1701 <122221954+mmax1701@users.noreply.github.com>
2024-11-26 21:34:27 +0200

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  createContact = e => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <ContactForm createContact={this.createContact} />
+
+        <h2>Contacts</h2>
+        <ContactList />
+      </>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export class ContactList extends Component {
+  render() {
+    return (
+      <ul>
+        <li></li>
+      </ul>
+    );
+  }
+}
